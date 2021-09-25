ab4f981f25a9fb67d067caa4308224afdd0b24aa
Md. Mahfuj Hasan <mahfuj170142@diit.edu.bd>
2021-09-25 10:07:02 +0600

    added unavilable feature

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-document.getElementById('search-btn').addEventListener('click', function () {
-    const searchText = document.getElementById('search-bar').value;
-    fetch(``)
-        .then(res => res.json())
-        .then(data => console.log(data))
-})
+// document.getElementById('search-btn').addEventListener('click', function () {
+//     const searchText = document.getElementById('search-bar').value;
+//     fetch(``)
+//         .then(res => res.json())
+//         .then(data => console.log(data))
+// })
+const unavilableFeature = () => {
+    alert("This feature is not available yet, please check back in the future.");
+}
