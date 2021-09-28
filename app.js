a6d1193c7833b433eb5319d5a50153c77446e02e
Md. Mahfuj Hasan <mahfuj170142@diit.edu.bd>
2021-09-28 15:54:20 +0600

    added top 10 readers section

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+// alert message 
 const unavilableFeature = () => {
     alert("This feature is not available yet, please check back in the future.");
+}
+
+// fetch user
+const url = `https://jsonplaceholder.typicode.com/users`
+fetch(url)
+    .then(res => res.json())
+    .then(data => displayUser(data))
+
+const displayUser = users => {
+    const userInfo = document.getElementById('user-info');
+    for (const user of users) {
+        const p = document.createElement('p');
+        p.innerText = `
+        Rank: ${user.id}
+        Name: ${user.name}
+        Email: ${user.email}
+        Phone: ${user.phone}
+        Website: ${user.website}
+        Company: ${user.company.name}
+        `
+        p.classList.add('col-4');
+        p.classList.add('user-container');
+        userInfo.appendChild(p);
+    }
 }
